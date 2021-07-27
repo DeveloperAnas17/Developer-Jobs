9cf43fcf8cada11f20da62e8f90c19c551ea604e
Anas <amanabbasi3660@gmail.com>
2021-07-28 01:55:53 +0530

    fetch json data

--- js/main.js
+++ js/main.js
@@ -4,19 +4,52 @@ document.querySelector(".button-container").addEventListener("click", () => {
 });
 
 function getJobs() {
- return fetch("data.json")
+  return fetch("data.json")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       return data;
     });
 }
 
 function showJobs(jobs) {
+  console.log(jobs);
+  let jobsContainer = document.querySelector(".jobs-container");
+  let jobsHTML = "";
+  jobs.forEach((job) => {
+    console.log(job.logo);
 
+    jobsHTML += `
+    <div class="job-tile">
+    <div class="top">
+      <img
+        src="${job.logo}"
+        alt=""
+      />
+      <span class="material-icons more_horiz">
+        <i class="fas fa-ellipsis-h"></i>
+      </span>
+    </div>
+    <div class="rolename">
+      <span>${job.roleName}</span>
+    </div>
+    <div class="description">
+      <span
+        >${job.requirements.content}</span
+      >
+    </div>
+    <div class="buttons">
+      <div class="button apply-now">Apply Now</div>
+      <div class="button">Message</div>
+    </div>
+  </div>
+    `;
+  });
+
+  console.log(jobsHTML);
+
+  jobsContainer.innerHTML = jobsHTML;
 }
 
-
-getJobs().then(data =>) {
-    showJobs(jobs)
-}
+getJobs().then((data) => {
+  showJobs(data);
+});
